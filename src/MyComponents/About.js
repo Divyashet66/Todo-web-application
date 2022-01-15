e3769e9b931f9e6403778d7204026951a22d1327
Divyashet66 <divyagurunathshet@gmail.com>
2022-01-16 00:22:49 +0530

    initail commit

---
+++ src/MyComponents/About.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const About = () => {
+    return (
+        <div>
+            This is an about component
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci provident voluptas ea temporibus animi dolor vel suscipit non distinctio et doloremque, delectus id, fugiat ipsum culpa eos. Earum dicta iure vitae mollitia tempora autem.</p>
+        </div>
+    )
+}
